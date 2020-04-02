edf5c17ccfb61ebe1ed3c5dbe013b5d5d25e6436
LittleScooter <55078146+LittleScooter@users.noreply.github.com>
2020-04-02 20:53:43 +0200

    started with svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
 	export let name;
+	import Header from "./Header.svelte";
 </script>
 
 <main>
+	<Header/>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
---
+++ src/Header.svelte
@@ -0,0 +1,20 @@
+<main>
+<button id="headBtn">Log in</button>
+<button id="headBtn">Sign up</button>
+</main>
+
+<style>
+    main
+    {
+        width: 100%;
+        height: 100px;
+        background-color: #d62943;
+        margin: none;
+    }
+    #headBtn
+    {
+        float: right;
+        margin-right: 5px;
+        margin-top: 5px;
+    }
+</style>
