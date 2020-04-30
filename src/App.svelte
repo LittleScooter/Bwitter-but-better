d3f10f107438a5f548ce89339e5149747844e423
Hugo Persson <hugopersson7@outlook.com>
2020-04-30 12:22:42 +0200

    routing example

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,55 @@
 <script>
-	export let name;
-	import Header from "./Header.svelte";
-	import FeedMenu from "./FeedMenu.svelte";
-	import Feed from "./Feed.svelte";
+  export let name;
+  import Header from "./Header.svelte";
+  import FeedMenu from "./FeedMenu.svelte";
+  import Feed from "./Feed.svelte";
+  import PersonalFeed from "./PersonalFeed.svelte";
+  import GlobalFeed from "./GlobalFeed.svelte";
+
+  let pageName = "";
+
+  window.onhashchange = () => (pageName = location.hash);
 </script>
 
-<main>
-	<Header/>
-	<FeedMenu/>
-	<Feed/>	
-</main>
-
 <style>
-	main {
-		text-align: center;
-		max-width: 240px;
-		padding: 0;
-		margin: 0 auto;
-		background-color: #3f3f40;
-	}
+  main {
+    text-align: center;
+    max-width: 240px;
+    padding: 0;
+    margin: 0 auto;
+    background-color: #3f3f40;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+
+  {#if pageName == '#global'}
+    <Header />
+    <FeedMenu />
+    <GlobalFeed />
+  {:else if pageName == '#personal'}
+    <Header />
+    <FeedMenu />
+    <PersonalFeed />
+  {:else if pageName == '#login'}
+    <Login />
+  {:else}
+    <Header />
+    <FeedMenu />
+    <Feed />
+  {/if}
+
+</main>
--- src/FeedMenu.svelte
+++ src/FeedMenu.svelte
@@ -1,15 +1,15 @@
-<main>
-<button id="PerFeed">Personal Feed</button>
-<button id="GlobFeed">Global Feed</button>
-</main>
-
 <style>
-    main{
-        width: 45%;
-        margin: auto;
-        margin-top: 10px; 
-    }
-    #PerFeed{
-        float: left;
-    }
+  main {
+    width: 45%;
+    margin: auto;
+    margin-top: 10px;
+  }
+  #PerFeed {
+    float: left;
+  }
 </style>
+
+<main>
+  <a id="PerFeed" href="#personal">Personal Feed</a>
+  <a id="GlobFeed" href="#global">Global Feed</a>
+</main>
